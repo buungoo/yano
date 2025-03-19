632b2848faa8ace56b4120bccd8519c34a2f75a2
Bungo <alxber-0@student.ltu.se>
2025-03-19 18:51:33 +0100

    immich

--- hosts/common.nix
+++ hosts/common.nix
@@ -49,6 +49,9 @@
 		host = "0.0.0.0";
 		mediaLocation = "/storage/immich";
 	};
+	# Ensures directory is created with 755 permissions
+	systemd.tmpfiles.rules = [ "d /storage/immich 0755 immich immich -" ];
+
 	networking.firewall.allowedTCPPorts = [ 2283 ];
 
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
