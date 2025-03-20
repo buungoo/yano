1b5b75b0936e1365d53193022e8877da447e5a16
Bungo <alxber-0@student.ltu.se>
2025-03-20 17:31:10 +0100

    immich container netwokr

--- hosts/immich2.nix
+++ hosts/immich2.nix
@@ -4,14 +4,17 @@
 	# --- Immich Container Configuration ---
 	containers.immich2 = {
 		autoStart = true;
+		privateNetwork = true;
+		hostAddress = "192.168.1.192";
+		localAddress = "192.168.1.8";
 		config = {
-			services.postgresql = {
-				enable = true;
-				settings = {
-					port = 5433;  # Change the PostgreSQL port inside the container
-					# listen_addresses = "127.0.0.1";  # Restrict to localhost inside the container
-				};
-			};
+			# services.postgresql = {
+			# 	enable = true;
+			# 	settings = {
+			# 		port = 5433;  # Change the PostgreSQL port inside the container
+			# 		# listen_addresses = "127.0.0.1";  # Restrict to localhost inside the container
+			# 	};
+			# };
 
 			# Immich service configuration inside the container.
 			services.immich = {
