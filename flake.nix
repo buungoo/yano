86d876c083e62e3fa253a418e101c41c574dafcd
Bungo <alxber-0@student.ltu.se>
2025-03-21 14:51:20 +0100

    mkconfig

--- flake.nix
+++ flake.nix
@@ -1,46 +1,55 @@
 {
-# NOTE:
-# sudo nixos-generate-config --show-hardware-config > hosts/<hostname>/hardware-configuration.nix
-	description = "NixOS Configuration with Flakes";
+  # NOTE:
+  # sudo nixos-generate-config --show-hardware-config > hosts/<hostname>/hardware-configuration.nix
+  description = "NixOS Configuration with Flakes";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-		nixosConfigurations = {
-			# Build with: sudo nixos-rebuild switch --flake .#nas0
-			nas0 = nixpkgs.lib.nixosSystem {
-				system = "x86_64-linux";
-				modules = [
-					./hosts/nas0/default.nix
-					./hosts/common.nix
-					home-manager.nixosModules.home-manager
-					{
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
-						home-manager.users.bungo = import ./home/bungo.nix;
-					}
-				];
-			};
-			# machine2 = nixpkgs.lib.nixosSystem {
-			# 	system = "x86_64-linux";
-			# 	modules = [
-			# 		./hosts/machine2/default.nix
-			# 		./hosts/common.nix
-			# 		home-manager.nixosModules.home-manager
-			# 		{
-			# 			home-manager.useGlobalPkgs = true;
-			# 			home-manager.useUserPackages = true;
-			# 			home-manager.users.user1 = import ./home/bungo.nix;
-			# 		}
-			# 	];
-			# };
-		};
-	};
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      inherit (self) outputs;
+      mkNixOSConfig = path:
+        nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs vars; };
+          modules = [ path ];
+        };
+    in
+    {
+      nixosConfigurations = {
+        # Build with: sudo nixos-rebuild switch --flake .#nas0
+        dev = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./hosts/nas0/default.nix
+            ./hosts/common.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.bungo = import ./home/bungo.nix;
+            }
+          ];
+        };
+        # machine2 = nixpkgs.lib.nixosSystem {
+        # 	system = "x86_64-linux";
+        # 	modules = [
+        # 		./hosts/machine2/default.nix
+        # 		./hosts/common.nix
+        # 		home-manager.nixosModules.home-manager
+        # 		{
+        # 			home-manager.useGlobalPkgs = true;
+        # 			home-manager.useUserPackages = true;
+        # 			home-manager.users.user1 = import ./home/bungo.nix;
+        # 		}
+        # 	];
+        # };
+      };
+    };
 }
